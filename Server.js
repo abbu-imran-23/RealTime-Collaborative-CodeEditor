a82270567b691a1525af3eba76faa41357584a0c
Imran <abbuimran02@gmail.com>
2023-12-28 21:39:54 +0530

    Changed PORT Numbers

--- Server.js
+++ Server.js
@@ -81,7 +81,7 @@ io.on("connection", (socket) => {
     });
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 server.listen(PORT, () => {
     console.log(`Server listening at PORT ${PORT}`);
 })
